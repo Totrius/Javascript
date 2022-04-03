d5bcbbe91acfb2e2b930b00036d1a0502aa26f54
KGrzeg <kupczykgrzeg@gmail.com>
2022-04-03 22:59:51 +0200

    i reszta tyż

--- 3/wisielec.js
+++ 3/wisielec.js
@@ -16,7 +16,11 @@ const baza = [
 	"błąd paralaksy",
 ];
 
-window.onload = start;
+const liczba_prob = 9;
+
+// window.onload = start;
+window.addEventListener("DOMContentLoaded", start);
+
 // let haslo = baza[Math.floor(Math.random()*14)];
 let haslo = baza[Math.floor(Math.random() * baza.length)];
 haslo = haslo.toUpperCase();
@@ -57,25 +61,38 @@ const litery = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ".split("");
 
 // koniec na dziś
 
+moje_guziki = [];
 function start()
 {
-	
 	let tresc_diva = "";
-	
-	for (i=0; i<=34; i++)
+
+	// for (i=0; i<=34; i++)
+	for (let i=0; i<=34; i++)
 	{
-		let element = "lit" + i;
-		tresc_diva = tresc_diva + '<div id="'+element+'" class="litera" onclick="sprawdz('+i+')">' + litery[i] + '</div>';
+		const element = "lit" + i;
+		// let element = "lit" + i;
+
+		// tresc_diva = tresc_diva + '<div id="'+element+'" class="litera" onclick="sprawdz('+i+')">' + litery[i] + '</div>';
+				tresc_diva = tresc_diva + `<div id="${element}" class="litera" onclick="sprawdz(${i})">${litery[i]}</div>`; //template literal
+
+
 		if ((i+1)%7 == 0)
 		{
 			tresc_diva = tresc_diva + '<div style="clear: both;"></div>';
 		}
 	}
-	
+
 	document.getElementById("klawiatura").innerHTML = tresc_diva;
 	wypisz_haslo();
+	
+	document.getElementById("chowaj_popup").addEventListener("click", function(){
+		document.getElementById("popup").style = "display: none";
+	})
 }
 
+
+
+//NIE WOLNO TAK ROBIĆ!@@@@!!!!
 String.prototype.ustawZnak = function(miejsce, znak)
 {
 	if (miejsce > this.length - 1)
@@ -85,15 +102,16 @@ String.prototype.ustawZnak = function(miejsce, znak)
 	else 
 	{
 		return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+		// return `${this.substr(0, miejsce)}${znak}${this.substr(miejsce+1)}`; //można, ale po co?
+		
 	}
 }
 
 function sprawdz(nr)
 {
-	
 	let trafiona = false;
 	
-	for(i=0; i<dlugosc; i++)
+	for(i=0; i<dlugosc; i++) // TODO: Znajdź błąd
 	{
 		if (haslo.charAt(i) == litery[nr])
 		{
@@ -104,12 +122,14 @@ function sprawdz(nr)
 	if	(trafiona == true)
 	{
 		dobrze.play();
-		let element = "lit" + nr;
+		const id_elementu = "lit" + nr;
+		const element = document.getElementById(id_elementu);
 	
-		document.getElementById(element).style.background = "#003300";
-		document.getElementById(element).style.color = "#00C000";
-		document.getElementById(element).style.border = "3px solid #00C000";
-		document.getElementById(element).style.cursor = "default";
+		//można zastosować klasę CSS
+		element.style.background = "#003300";
+		element.style.color = "#00C000";
+		element.style.border = "3px solid #00C000";
+		element.style.cursor = "default";
 		
 		wypisz_haslo();
 	}
@@ -126,16 +146,24 @@ function sprawdz(nr)
 		ile_skuch++;
 		let obraz = "img/s" + ile_skuch + ".jpg"
 		document.getElementById("szubienica").innerHTML = '<img src="' + obraz + '"alt=""/>'
+		//document.getElementById("obrazek").src = obraz; //lepiej tak! (tylko dodaj atrybut id do obrazka)
 		
 	}
 	//wygrana
 	if (haslo == ukryteHaslo)
 	{
 		document.getElementById("klawiatura").innerHTML = "Tak jest! Podano prawidłowe hasło! " + haslo + '<br/><br/><span class="reset" onclick="location.reload()"> JESZCZE RAZ??</span>';
+		/* gotowy kawałek w HTMLu który tylko się pokazuje (na brudno)
+		document.getElementById("wygrana").style ="";
+		document.getElementById("haslo").innerText = haslo;
+		document.getElementById("klawiatura").style = "display:none";
+		*/
 	}
 	//porażka
-	if (ile_skuch >= 9)
+	if (ile_skuch >= liczba_prob)
 	{
 		document.getElementById("klawiatura").innerHTML = "Przrgrana, nie udało się zgadnąć hasła: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()"> JESZCZE RAZ??</span>';
 	}
 }
+
+
