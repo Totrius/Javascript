352da329b8f244cc070097b7e8499afdc2a83bf8
Totrius <57675033+Totrius@users.noreply.github.com>
2022-04-09 23:07:41 +0200

    nowa gra bez przeładowania

--- 3/wisielec.js
+++ 3/wisielec.js
@@ -70,40 +70,35 @@ const baza = [
  "zootechnika",
  "psychologia",
 ];
-window.addEventListener("DOMContentLoaded", start);
-let haslo = baza[Math.floor(Math.random()*baza.length)];
-haslo = haslo.toUpperCase();
 const liczbaProb = 9;
-let dlugosc = haslo.length;
 let ukryteHaslo = "";
 let ile_skuch = 0;
-
+const litery = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ".split("");
 const dobrze = new Audio("dobrze.wav");
 const zle = new Audio("zle.wav");
+let haslo = "";
+let dlugosc = 0;
+window.addEventListener("DOMContentLoaded", start);
 
-for (let i=0; i<dlugosc; i++)
+
+function reset()
 {
-	if(haslo.charAt(i) == " ") 
-	{
-		ukryteHaslo = ukryteHaslo + " ";
-	}
-	else 
-	{
-		ukryteHaslo = ukryteHaslo + "_";
-	}
+	ile_skuch = 0;
+	haslo = "";
+	ukryteHaslo="";
+	dlugosc = 0;
+	document.getElementById("szubienica").innerHTML = `<img src="img/s0.jpg" alt=""/>`;
 }
 
-function wypisz_haslo()
-{
-	document.getElementById("plansza").innerText = ukryteHaslo;
-}
-
-
-
-const litery = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ".split("");
 
 function start()
 {
+	reset();
+	haslo = baza[Math.floor(Math.random()*baza.length)];
+	haslo = haslo.toUpperCase();
+	dlugosc = haslo.length;
+	
+	
 	
 	let tresc_diva = "";
 	
@@ -118,9 +113,28 @@ function start()
 	}
 	
 	document.getElementById("klawiatura").innerHTML = tresc_diva;
+	ukrywanie();
 	wypisz_haslo();
 }
 
+function ukrywanie()
+{
+	for (let i=0; i<dlugosc; i++)
+	{
+		if(haslo.charAt(i) == " ") 
+		{
+			ukryteHaslo = ukryteHaslo + " ";
+		}
+		else 
+		{
+			ukryteHaslo = ukryteHaslo + "_";
+		}
+	}
+}
+function wypisz_haslo()
+{
+	document.getElementById("plansza").innerText = ukryteHaslo;
+}
 String.prototype.ustawZnak = function(miejsce, znak)
 {
 	if (miejsce > this.length - 1)
@@ -172,17 +186,17 @@ function sprawdz(nr)
 		
 		ile_skuch++;
 		let obraz = "img/s" + ile_skuch + ".jpg"
-		document.getElementById("szubienica").innerHTML = `<img src="${obraz}"alt=""/>`
+		document.getElementById("szubienica").innerHTML = `<img src="${obraz}"alt=""/>`;
 		
 	}
 	//wygrana
 	if (haslo == ukryteHaslo)
 	{
-		document.getElementById("klawiatura").innerHTML = "Tak jest! Podano prawidłowe hasło! " + haslo + '<br/><br/><span class="reset" onclick="location.reload()"> JESZCZE RAZ??</span>';
+		document.getElementById("klawiatura").innerHTML = "Tak jest! Podano prawidłowe hasło! " + haslo + '<br/><br/><span class="reset" onclick="start()"> JESZCZE RAZ??</span>';
 	}
 	//porażka
 	if (ile_skuch >= liczbaProb)
 	{
-		document.getElementById("klawiatura").innerHTML = "Przrgrana, nie udało się zgadnąć hasła: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()"> JESZCZE RAZ??</span>';
+		document.getElementById("klawiatura").innerHTML = "Przrgrana, nie udało się zgadnąć hasła: " + haslo + '<br/><br/><span class="reset" onclick="start()"> JESZCZE RAZ??</span>';
 	}
 }
