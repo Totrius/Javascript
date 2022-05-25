a04b8584a334669311da1b516f6e47265ba6b127
Totrius <totrius123@gmail.com>
2022-05-25 19:51:56 +0200

    początek gry w pamiec

---
+++ gra_w_pamiec/memory.js
@@ -0,0 +1,8 @@
+const liczbaKart = 12;
+
+let karty = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
+
+function odslonKarty() 
+{
+    
+}
